eec0b1b03b22b8467948b1131154df3c7e237e8e
Edem Kestan <ed.kestan@gmail.com>
2024-03-18 17:26:42 +0200

    task

---
+++ todo-list2/src/list/updateTask.js
@@ -0,0 +1,22 @@
+import { renderTasks } from './renderer.js';
+import { updateTaskById } from './tasks.js';
+
+export const onToggleTask = e => {
+    const isCheckbox = e.target.classList.contains('list__item-checkbox');
+
+    if (!isCheckbox) {
+        return;
+    }
+
+    const taskId = e.target.dataset.id;
+    const done = e.target.checked;
+
+    const updatedTask = {
+        done,
+        finishDate: done ? new Date().toISOString() : null
+    };
+
+    updateTaskById(taskId, updatedTask)
+        .then(() => renderTasks())
+        .catch(error => console.error('Error updating task:', error));
+};
---
+++ todo-list2/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
